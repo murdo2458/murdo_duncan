756e6dce068cf0ab9e3499fc2f6ab9500f7c5e7e
murd2458 <murdo124@gmail.com>
2024-05-23 00:28:12 +0100

    analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image, { type ImageProps } from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
+import { Analytics } from "@vercel/analytics/react"
+<Analytics />
 
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
